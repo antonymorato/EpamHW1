960d89bb3acad936dfd6c402f33b2d6048132c7a
antony <antonymorato2@gmail.com>
2019-03-24 11:42:26 +0200

    added files

---
+++ Task2/root/src/com/epam/courses2/Hello2.java
@@ -0,0 +1,16 @@
+
+package com.epam.courses2;
+
+
+import com.epam.courses.Hello;
+
+
+public class Hello2{
+
+    public static void main(String[] args)
+    {
+            Hello var=new Hello();
+
+
+    }
+}
